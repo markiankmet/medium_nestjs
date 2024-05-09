5f57323c3b0ee87c7547e064b41cedf42ab90030
markiankmet <markkmet13@gmail.com>
2024-05-09 23:33:27 +0300

    add tagEntity, add tagRepository to work in TagService

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as process from 'process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: { database: PostgresConnectionOptions } = {
+  database: {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: +process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  },
+};
+
+export default config;
